f3550f83a906c8bebea1677803944a6bf3ea23d0
phoitack <phoitack@gmail.com>
2020-09-05 17:06:18 -0700

    Some SQL problems

---
+++ SQL/596_Classes_more_than_5_students.sql
@@ -0,0 +1,38 @@
+/*
+
+There is a table courses with columns: student and class
+
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+Should output:
+
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+
+
+*/
+
+SELECT class
+FROM
+(SELECT class, COUNT(student) as num_students
+FROM courses
+GROUP BY class) temp
+WHERE num_students >= 5;
